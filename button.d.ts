16e90acc86c940aeb01dc8754263858a93a49c2a
Martin Ouimet <mouimet@infini-soft.com>
2022-05-20 22:50:37 -0400

    typo fixed

--- button.d.ts
+++ button.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="react" />
 
 declare module '{{{name}}}/{{{component}}}' {
-  export type { };
-{component}}Props = {
+  export type {{component}}Props = {
     message?: string;
   };
   const {{component}}: ({ message }: {{component}}Props) => JSX.Element;
