c714c5937ecc47413ee88de61745a5ae1d45278b
Martin Ouimet <mouimet@infini-soft.com>
2022-05-24 22:41:39 -0400

    remove unused types declaration

--- button.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-
-declare module '{{{name}}}/{{{component}}}' {
-  export type {{component}}Props = {
-    message?: string;
-  };
-  const {{component}}: ({ message }: {{component}}Props) => JSX.Element;
-  export default {{component}};
-}
