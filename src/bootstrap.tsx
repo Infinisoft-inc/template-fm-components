168cd736a792cae2f7c21afb425a11170bb5dcb7
Martin Ouimet <mouimet@infini-soft.com>
2022-05-26 17:31:21 -0400

    code clean up and copyright

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,5 +1,18 @@
+/*
+ * Copyright © All rights reserved 2022
+ * Infinisoft Inc.
+ * www.infini-soft.com
+ */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Component from './component';
 
+/**
+ * Component is fetched at runtime with Module Federation.
+ * Therefore, props are unknown at compile time.
+ *
+ * ts-ignore is required to silent Typescript error
+ */
+
+//@ts-ignore
 createRoot(document.getElementById('root')!).render(<Component />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
+/*
+ * Copyright © All rights reserved 2022
+ * Infinisoft Inc.
+ * www.infini-soft.com
+ */
 import('./bootstrap')
