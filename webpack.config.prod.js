566eb00f9c304153440d70ccd3fd9206bcb1e0b8
Martin Ouimet <mouimet@infini-soft.com>
2022-05-26 16:18:50 -0400

    update component name transform

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,7 +15,7 @@ module.exports = merge(common, {
       name,
       filename: 'remoteEntry.js',
       exposes: {
-        [infinisoft.moduleFederation.component]: './src/component',
+        [`./${infinisoft.moduleFederation.component}`]: './src/component',
       },
       shared: {
         ...peerDependencies,
