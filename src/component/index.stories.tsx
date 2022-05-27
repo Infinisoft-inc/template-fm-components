f4bc61e70122cc5522e710a141b4bf6399bf4d1a
Martin Ouimet <mouimet@infini-soft.com>
2022-05-27 02:29:43 -0400

    rollback to tsx. mdx cant work for now with federation and lazy loa ding

---
+++ src/component/index.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import {{{component}}}} from '.';
+
+export default {
+  title: 'TEMPLATE/{{{component}}}',
+  component: {{{component}}},
+} as ComponentMeta<typeof {{{component}}}>;
+
+const Template: ComponentStory<typeof {{{component}}}> = (args) => <{{{component}}} {...args} />;
+
+export const StringArgs = Template.bind({});
+StringArgs.args = {
+  title: 'Title',
+  content: 'Content'
+};
