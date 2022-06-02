e39ca86b6f41f153571cf4ef2a27d05ed3e040a6
Martin Ouimet <mouimet@infini-soft.com>
2022-06-02 04:14:31 -0400

    refactor story template

--- src/component/index.stories.tsx
+++ src/component/index.stories.tsx
@@ -5,12 +5,60 @@ import {{{component}}} from '.';
 export default {
   title: 'TEMPLATE/{{{component}}}',
   component: {{{component}}},
+  argTypes: {
+    content: {
+      defaultValue: '',
+      description: 'overwritten description',
+      table: {
+        category: 'Content',
+        type: {
+          summary: 'something short',
+          detail: 'something really really long',
+        },
+        defaultValue: { summary: 'Hello' },
+      },
+      control: {
+        type: null,
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: 'This is a super cool button',
+      },
+    },
+  },
 } as ComponentMeta<typeof {{{component}}}>;
 
-const Template: ComponentStory<typeof {{{component}}}> = (args) => <{{{component}}} {...args} />;
+const Template: ComponentStory<typeof {{{component}}}> = (args) => (
+  <{{{component}}} {...args}/>
+);
 
 export const StringArgs = Template.bind({});
 StringArgs.args = {
   title: 'Title',
-  content: 'Content'
+  content: 'Content',
+};
+
+StringArgs.parameters = {
+  docs: {
+    description: {
+      story: 'This is a string',
+    },
+  },
+};
+
+export const StringArgs2 = Template.bind({});
+StringArgs2.args = {
+  title: 'Title',
+  content: 'Content',
+};
+
+StringArgs2.parameters = {
+  docs: {
+    description: {
+      story: 'This is a string2',
+    },
+  },
 };
